15832d1037e2808147b9d511370bb4b005484ad5
Nikobog <74555133+Nikobog@users.noreply.github.com>
2021-02-04 20:10:53 +0200

    Create constans.js

---
+++ scr/pages/constans.js
@@ -0,0 +1,27 @@
+export const days = [
+  { id: "d1", title: "Mon" },
+  { id: "d2", title: "Tue" },
+  { id: "d3", title: "Wed" },
+  { id: "d4", title: "Thu" },
+  { id: "d5", title: "Fri" }
+];
+
+export const times = [
+  { id: "t1", title: "10:00" },
+  { id: "t2", title: "11:00" },
+  { id: "t3", title: "12:00" },
+  { id: "t4", title: "13:00" },
+  { id: "t5", title: "14:00" },
+  { id: "t6", title: "15:00" },
+  { id: "t7", title: "16:00" },
+  { id: "t8", title: "17:00" },
+  { id: "t9", title: "18:00" }
+];
+
+export const users = [
+  { id: "u1", title: "Nik", meeting: ["m15, m32"] },
+  { id: "u2", title: "Alex", meeting: ["m27, m32"] },
+  { id: "u3", title: "Sunny", meeting: ["m27, m32"] },
+  { id: "u4", title: "Bob", meeting: ["m15, m27"] },
+  { id: "u5", title: "Jon", meeting: ["m15, m32"] }
+];
